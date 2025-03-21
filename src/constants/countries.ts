296ed6a4fd1880f1f21130a06dd3ca23650c6950
Henrique Marques <hmdev132@gmail.com>
2025-03-21 13:47:18 -0300

    feat(constants): add countries data

---
+++ src/constants/countries.ts
@@ -0,0 +1,46 @@
+export const COMMON_COUNTRIES = [
+    {value: 'Brazil', label: 'Brasil'},
+    {value: 'Canada', label: 'Canadá'},
+    {value: 'United States', label: 'Estados Unidos'},
+    {value: 'Portugal', label: 'Portugal'},
+    {value: 'Spain', label: 'Espanha'},
+    {value: '', label: 'Todos'}
+  ];
+  
+  export const ALL_COUNTRIES = [
+    { label: 'Austrália', value: 'Australia' },
+    { label: 'Brasil', value: 'Brazil' },
+    { label: 'Canadá', value: 'Canada' },
+    { label: 'China', value: 'China' },
+    { label: 'França', value: 'France' },
+    { label: 'Alemanha', value: 'Germany' },
+    { label: 'Índia', value: 'India' },
+    { label: 'Indonésia', value: 'Indonesia' },
+    { label: 'Japão', value: 'Japan' },
+    { label: 'México', value: 'Mexico' },
+    { label: 'Polônia', value: 'Poland' },
+    { label: 'Rússia', value: 'Russia' },
+    { label: 'Turquia', value: 'Turkey' },
+    { label: 'Estados Unidos', value: 'United States' },
+  ];
+  
+  export const COUNTRY_CODES: { [key: string]: string } = {
+    'Australia': 'AU',
+    'Brazil': 'BR',
+    'Canada': 'CA',
+    'China': 'CN',
+    'France': 'FR',
+    'Germany': 'DE',
+    'India': 'IN',
+    'Indonesia': 'ID',
+    'Japan': 'JP',
+    'Mexico': 'MX',
+    'Poland': 'PL',
+    'Russia': 'RU',
+    'Turkey': 'TR',
+    'United Kingdom': 'GB',
+    'United States': 'US',
+    'Portugal': 'PT',
+    'Spain': 'ES',
+  };
+  
